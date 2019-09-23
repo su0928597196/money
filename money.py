b9e7d6eb1301837b7a3665b07ffda63ebd843752
su0928597196 <lintonyboy@yahoo.com.tw>
2019-09-24 03:10:00 +0800

    fix file

--- money.py
+++ money.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('+8ppuqlOtUzevqJMjXxkJ4ryUh661cgK4Ro2avASD25XektkpRyhpADvVqT1q0JZcpjsitF0ajqydFDi4u1EkuJ7PdNQhF7LsT94f8IdMAkjZgV1Ddw0WdvGxe9KrUTnA2WWTEWridENtmYhmun0QAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('41ad6acca52aa078eaeb0135e7a8ec9f')
+line_bot_api = LineBotApi('uNz4WmN6Ekhi32V+6397AVeQE03bSJU8k4RQ4FV9t6ZwDWYywABuNmT1255mwMSmYpsA5WeIc48LmlbrSDxY4hj47ySINwAO/OPLY0squlAOketT4DqfmS7zofY80u6rxFKCbdsO5biFey/2E7/OVAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('dab6186a76822a5207cb032d59caa752')
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,9 +37,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '媽媽說好的獎學金呢'
+    if msg in ['不', '沒有', '想太多']:
+        r = '說話不算話,週日我不去了'
+    elif msg in ['好', '好啦', '會給你']:
+        r = '好,下次我會幫忙顧婆婆的'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
