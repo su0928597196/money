62e61fdab6e84fd51f2f1ef1b27f1151724c8d1e
su0928597196 <lintonyboy@yahoo.com.tw>
2019-10-29 02:14:16 +0800

    update

--- money.py
+++ money.py
@@ -73,8 +73,8 @@ def handle_message(event):
         elif msg == '6':
             r = ''
             video_message = VideoSendMessage(
-            original_content_url = ' https://example.com/original.mp4 '，
-            preview_image_url = ' https://example.com/preview.jpg ' )
+            original_content_url = " https://i.imgur.com/RG6zFTv.mp4 ",
+            preview_image_url = " https://i.imgur.com/RG6zFTv.jpg " )
             line_bot_api.reply_message(event.reply_token, video_message)
 ）
         
