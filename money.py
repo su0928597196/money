1eb2fe78af83abaebc90e97ec422226f658fe099
su0928597196 <lintonyboy@yahoo.com.tw>
2019-10-28 23:59:48 +0800

    update

--- money.py
+++ money.py
@@ -60,7 +60,10 @@ def handle_message(event):
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
-
+        sticker_message = StickerSendMessage(package_id='11537',sticker_id='52002734')
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
 
 if __name__ == "__main__":
     app.run()
