6e72001f404f0603ff3d65b208b07d11cd149ff1
su0928597196 <lintonyboy@yahoo.com.tw>
2019-10-28 21:50:38 +0800

    update

--- money.py
+++ money.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,StickerSendMessage
 )
-import ramdon
+import random
 
 app = Flask(__name__)
 
